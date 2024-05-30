0ce4e8c01954985b476730e6174f13ce51032491
gugee0 <gugeshaa0@gmail.com>
2024-05-30 22:55:58 +0400

    commit

---
+++ level 5/level5.py
@@ -0,0 +1,30 @@
+name='Mariami'
+print(name)     
+
+#my full namename
+
+last_name='gugeshashvili'
+print(last_name)
+
+#my last name
+
+
+age=13
+print(13)
+
+#my age
+
+hobby='watching movies'
+print(hobby)
+
+#my favorite hobby
+
+
+country='georgia'
+print(country)
+
+#country that im from 
+
+print(name,last_name, hobby, country)
+
+#all printed together
