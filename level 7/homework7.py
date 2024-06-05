f42c98e32c1d787f809404940acfb2e9a6467627
gugee0 <gugeshaa0@gmail.com>
2024-06-05 19:55:49 +0400

    Create homework7.py

---
+++ level 7/homework7.py
@@ -0,0 +1,13 @@
+# name = 'mari'
+# print(name)
+
+# name = input("mari")
+# last_name = input("gugeshashvili")
+# age = input(13)
+
+# country = input ('georgia')
+# favorite color = input ('green')
+# favorite number= inpuut ('3')
+
+# print('name', 'last_name', 'age', 'country', 'favorite color', 'favorite number')
+
