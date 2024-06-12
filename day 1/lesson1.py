370a6f86bddea48a58fd8545e15dd79c7cdcd902
gugee0 <gugeshaa0@gmail.com>
2024-06-12 17:43:07 +0400

    commit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -2,5 +2,3 @@ from turtle import *
 
 #we want to paint a house
 #step 1: draw a square
-
-wi
---
+++ level 9/level9.py
@@ -0,0 +1,72 @@
+#welcome message
+print("welcome to the basic calculator please enter numbers ;)")
+
+# Input for the first number
+num1 = float(input("Enter the first number: "))
+
+# Input for the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate
+print("The sum is:", num1 + num2)
+
+
+
+
+
+#message 
+print("now you have to enter length and width ;V ")
+
+# Input for the width
+width = float(input("Enter the width: "))
+
+# Input for the length
+length = float(input("Enter the length: "))
+
+#calculate
+area = width * length
+
+
+
+#message
+print("perfect! you can also write more numbers and let us multiply it ;p ")
+
+# Input for the first number
+width = float(input("Enter the first number: "))
+
+# Input for the second number
+length = float(input("Enter the second number: "))
+
+#calculate 
+area = width * length
+
+
+
+#message
+print("good! one more time")
+
+# Input for the width
+width = float(input("Enter the width: "))
+
+# Input for the length
+length = float(input("Enter the length: "))
+
+
+#calculate
+total = num1 + num2
+
+
+
+#message
+print("well done now you have to enter two numbers and we will exponentiation it :^ ")
+
+# Input for the first number
+num1 = float(input("Enter the first number: "))
+
+# Input for the second number
+num2 = float(input("Enter the second number: "))
+
+
+#calculate
+num1_square = num1 ** 2
+num2_square = num2 ** 2
