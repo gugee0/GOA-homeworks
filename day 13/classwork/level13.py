c66e3d8490b6c274a53a2f34f9f304467e69d40b
gugee0 <gugeshaa0@gmail.com>
2024-06-27 19:38:08 +0400

    commit

---
+++ day 13/classwork/level13.py
@@ -0,0 +1,15 @@
+#The program takes the score as an input
+score=int(input(120))
+
+#add the comprasion operation inside the parentheses
+
+
+
+#take steps and minutes as inputs
+steps=int(input())
+active_minutes=int(input())
+
+#store the result of he operation in the variable
+
+#display the result on the screen
+print("goal_achieved")
---
+++ day 13/homework/level13.py
@@ -0,0 +1,47 @@
+# logical operators for example:
+
+#1
+x=5
+y=10
+print(x<y)
+
+#2
+x=15
+y=46
+print(x>y)
+
+#3
+print(1+1==3)
+
+#4
+print(2+1==3)
+
+#5
+print(2.4>4)
+
+#6
+print(2<1)
+
+#7
+print("coffe"=="milk")
+
+#8
+a=20
+b=30
+c=40
+print(a+b==c)
+print(c<a)
+
+#9
+name1="Ellie"
+name2="joel"
+
+print(name1==name2)
+print(len(name1) > 7)
+
+#10
+name="abby"
+age=19
+is_adult=age>=18
+
+print(is_adult and name== "abby")
