cf340f396488218ade572ecdd82ce827a94167cf
gugee0 <gugeshaa0@gmail.com>
2024-07-18 15:35:35 +0400

    commit

---
+++ day 19/homework/level19.py
@@ -0,0 +1,64 @@
+txt= 'Goa is the best'
+for i in txt:
+    print(i)
+
+
+txt= 'Group 25'
+for i in txt:
+    print(i)
+
+
+total = 0
+for i in range(1, 2000):
+    total += i
+print('sum:', total)
+
+
+for i in range(10, 1, -1):
+    print(i)
+
+X = 1
+X = X // 10
+print(X)
+
+total = 0
+for i in range(1, 5000):
+    total += i
+print('sum', total)
+
+
+txt= 'yiyliyo is the best ;>'
+for i in txt:
+    print(i)
+
+
+count = 14
+while count <=10: #false
+    print('hello')
+    count += 3
+
+
+txt= 'programming is on of the best ways to get successfull'
+for i in txt:
+    print(i)
+
+while True:
+    print('hello goa')
+
+
+total = 0
+for i in range(1, 8000):
+    total += i 
+print('sum', total)
+
+
+
+count = 15
+while count <=10: #false
+    print('hello')
+    count += 3
+    
+X = 1
+X = X // 9
+print(X)
+
