64533cb4e8dea6081da03f913e252fb047754fca
gugee0 <gugeshaa0@gmail.com>
2024-08-14 17:45:17 +0400

    commit

---
+++ day 27/homework/level27.py
@@ -0,0 +1,7 @@
+my_list = [42, 3.14, "hello, goa!", True, [1, 2, 3], (4, 5, 6), {'a': 1}, {7, 8, 9}, None, lambda x: x + 1]
+my_list[0] = 100        
+my_list[1] = 2.71      
+my_list[2] = "goa" 
+my_list[3] = False     
+my_list[4] = [10, 20, 30]
+print(my_list)
