bc3d6025cd8a6b0084ca034fcd195af6dfcfe760
gugee0 <gugeshaa0@gmail.com>
2024-08-30 15:34:16 +0400

    commit

---
+++ day 33/homework/level33.py
@@ -0,0 +1,11 @@
+def simple_calculator(num1, num2, operation):
+    if operation == 'addition':
+        return num1 + num2
+    elif operation == 'subtraction':
+        return num1 - num2
+    elif operation == 'multiplication':
+        return num1 * num2
+    elif operation == 'division':
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        return num1 / num2
